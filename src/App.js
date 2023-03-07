a659760314aaf33f6bb299060c04219a9a428687
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-03-07 17:06:07 +0200

    feat: :sparkles: Add ColorBox

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import Palette from "./components/Palette";
-import seedColors from "./components/seedColors";
+import Palette from "./Palette";
+import seedColors from "./seedColors";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Palette {...seedColors[4]} />
+        <Palette {...seedColors[1]} />
       </div>
     );
   }
