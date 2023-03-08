807483746dd440fa9406c9a039252ba93e7333a0
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-03-08 14:42:58 +0200

    feat: :sparkles: Add select + material UI pkg

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { format: "hex" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ format: e.target.value });
+    this.props.handleChange(e.target.value);
+  }
+
   render() {
     const { level, changeLevel } = this.props;
+    const { format } = this.state;
     return (
       <header className="Navbar">
         <div className="logo">
           <a href="#N/A">React Color Picker</a>
         </div>
+
         <div className="slider-container">
           <span>Level: {level}</span>
           <div className="slider">
@@ -22,6 +37,14 @@ class Navbar extends Component {
               onChange={changeLevel}
             />
           </div>
+        </div>
+
+        <div className="select-container">
+          <Select value={format} onChange={this.handleChange}>
+            <MenuItem value="hex">HEX - #ffffff</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgba(255,255,255, 1.0)</MenuItem>
+          </Select>
         </div>
       </header>
     );
