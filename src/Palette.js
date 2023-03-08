70b91731e024094cf7ca12e9be2db0aad2cce185
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-03-08 14:17:31 +0200

    feat: :sparkles: Add Navbar and font

--- src/Palette.js
+++ src/Palette.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
 import "./Palette.css";
+import Navbar from "./Navbar";
 
 class Palette extends Component {
+  constructor(props) {
+    super(props);
+    // Make a default state to control it using <Slider/> component
+    this.state = { level: 500 };
+    this.changeLevel = this.changeLevel.bind(this);
+  }
+
+  changeLevel(level) {
+    this.setState({ level });
+  }
+
   render() {
-    const colorBoxes = this.props.colors.map(color => (
-      <ColorBox background={color.color} name={color.name} />
+    const { colors } = this.props.palette;
+    const { level } = this.state;
+    const colorBoxes = colors[level].map((color) => (
+      <ColorBox background={color.hex} name={color.name} />
     ));
     return (
-      <div className='Palette'>
+      <div className="Palette">
+        <Navbar level={level} changeLevel={this.changeLevel} />
         {/* Navbar goes here */}
-        <div className='Palette-colors'>{colorBoxes}</div>
+        <div className="Palette-colors">{colorBoxes}</div>
         {/* footer eventually */}
       </div>
     );
