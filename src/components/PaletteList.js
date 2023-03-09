fe1f0d2a206b2c5ff35a0a8c67cdbee58ef1a8d3
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-03-09 12:16:00 +0200

    style: :lipstick: Start styling MiniPalette

--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import MiniPalette from "./MiniPalette";
 
 class PaletteList extends Component {
   render() {
@@ -8,9 +8,7 @@ class PaletteList extends Component {
       <div>
         <h1>React Colors</h1>
         {palettes.map((palette) => (
-          <p>
-            <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-          </p>
+          <MiniPalette {...palette} />
         ))}
       </div>
     );
