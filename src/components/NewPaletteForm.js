86bcd7f6bbfdb4e3e966a0b7bf2fb415400c4cd3
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-03-11 12:43:31 +0200

    feat: :sparkles: Add SavePalette

--- src/components/NewPaletteForm.js
+++ src/components/NewPaletteForm.js
@@ -87,6 +87,7 @@ class NewPaletteForm extends Component {
     this.updateCurrentColor = this.updateCurrentColor.bind(this);
     this.addNewColor = this.addNewColor.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   componentDidMount() {
     ValidatorForm.addValidationRule("isColorNameUnique", (value) =>
@@ -118,8 +119,20 @@ class NewPaletteForm extends Component {
     };
     this.setState({ colors: [...this.state.colors, newColor], newName: "" });
   }
+
   handleChange(evt) {
     this.setState({ newName: evt.target.value });
+  }
+
+  handleSubmit() {
+    let newName = "New Test Palette";
+    const newPalette = {
+      paletteName: newName,
+      id: newName.toLowerCase().replace(/ /g, "-"),
+      colors: this.state.colors,
+    };
+    this.props.savePalette(newPalette);
+    this.props.history.push("/");
   }
 
   render() {
@@ -131,6 +144,7 @@ class NewPaletteForm extends Component {
         <CssBaseline />
         <AppBar
           position="fixed"
+          color="default"
           className={classNames(classes.appBar, {
             [classes.appBarShift]: open,
           })}
@@ -147,6 +161,13 @@ class NewPaletteForm extends Component {
             <Typography variant="h6" color="inherit" noWrap>
               Persistent drawer
             </Typography>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.handleSubmit}
+            >
+              Save Palette
+            </Button>
           </Toolbar>
         </AppBar>
         <Drawer
