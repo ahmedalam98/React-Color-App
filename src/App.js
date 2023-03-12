5dd91c02e3135e05b716e502f982a8772ac336a8
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-03-12 12:20:05 +0200

    feat: :sparkles: Save palettes to local storage

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import NewPaletteForm from "./components/NewPaletteForm";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { palettes: seedColors };
+    const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
+    this.state = { palettes: savedPalettes || seedColors };
     this.savePalette = this.savePalette.bind(this);
     this.findPalette = this.findPalette.bind(this);
   }
@@ -22,7 +23,17 @@ class App extends Component {
   }
 
   savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] });
+    this.setState(
+      { palettes: [...this.state.palettes, newPalette] },
+      this.syncLocalStorage
+    );
+  }
+  syncLocalStorage() {
+    //save palettes to local storage
+    window.localStorage.setItem(
+      "palettes",
+      JSON.stringify(this.state.palettes)
+    );
   }
 
   render() {
