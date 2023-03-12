44e276d00153b4059d4917702e41c71c46db23fe
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-03-12 12:20:49 +0200

    style: :lipstick: Add deleteIcon to MiniPalette

--- src/styles/JSS/MiniPaletteStyles.js
+++ src/styles/JSS/MiniPaletteStyles.js
@@ -5,8 +5,9 @@ const styles = {
     borderRadius: "5px",
     padding: "0.5rem",
     position: "relative",
-    "&:hover": {
-      cursor: "pointer",
+    cursor: "pointer",
+    "&:hover svg": {
+      opacity: 1,
     },
   },
   colors: {
@@ -37,6 +38,18 @@ const styles = {
     margin: "0 auto",
     position: "relative",
     marginBottom: "-4px",
+  },
+  deleteIcon: {
+    color: "white",
+    backgroundColor: "#eb3d30",
+    width: "20px",
+    height: "20px",
+    position: "absolute",
+    right: "0px",
+    top: "0px",
+    padding: "10px",
+    zIndex: 10,
+    opacity: 0,
   },
 };
 
