0c7ded3f67712c83dc198287a9ce47dac5205eaf
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-03-14 18:54:15 +0200

    feat: :sparkles: Prevent duplicate random colors

--- src/components/NewPaletteForm.js
+++ src/components/NewPaletteForm.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-loop-func */
 import React, { Component } from "react";
 import classNames from "classnames";
 import { withStyles } from "@material-ui/core/styles";
@@ -60,8 +61,16 @@ class NewPaletteForm extends Component {
   addRandomColor() {
     //pick random color from existing palettes
     const allColors = this.props.palettes.map((p) => p.colors).flat();
-    var rand = Math.floor(Math.random() * allColors.length);
-    const randomColor = allColors[rand];
+    let rand;
+    let randomColor;
+    let isDuplicateColor = true;
+    while (isDuplicateColor) {
+      rand = Math.floor(Math.random() * allColors.length);
+      randomColor = allColors[rand];
+      isDuplicateColor = this.state.colors.some(
+        (color) => color.name === randomColor.name
+      );
+    }
     this.setState({ colors: [...this.state.colors, randomColor] });
   }
 
